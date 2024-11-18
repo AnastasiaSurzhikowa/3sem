d44d5007bd5143d056d2e6d6be6ec74484591cea
Coffeein0 <danuha1005@mail.ru>
2024-11-18 07:05:18 +0300

    Добавлен парсинг рассписания гуап в тестовом режиме

--- Sait/main/urls.py
+++ Sait/main/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('about', views.about),
+    path('contact', views.schedule_view),
     path('testpic', views.testpic),
     path('calendar', views.calendar),
     path('mainpage', views.mainpage),
--- Sait/main/views.py
+++ Sait/main/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-
+import requests
+from bs4 import BeautifulSoup
 
 def about(request):
-    return render(request,'main/about.html')
+    return render(request, 'main/about.html')
 
 def testpic(request):
     return render(request, 'main/testpic.html')
@@ -14,4 +15,39 @@ def mainpage(request):
     return render(request, 'main/mainpage.html')
 
 def contact(request):
-    return render(request, 'main/contact.html')
+    return render(request, 'main/about.html')
+
+def schedule_view(request):
+    group_value = "317"
+    group_value1 = "12312"  # Инициализация переменной по умолчанию
+    
+    # Получаем группу пользователя
+    if request.user.is_authenticated:
+        group_value = request.user.group
+        group_value1 = str(group_value)
+    
+    url = "https://guap.ru/rasp/?g=" + group_value1
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+    }
+    
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()  # Проверяем на ошибку HTTP
+
+        soup = BeautifulSoup(response.text, 'html.parser')
+        result_element = soup.find(class_='result')
+
+        if result_element:
+            # Удаляем элементы с классом 'legend'
+            for legend in result_element.find_all(class_='legend'):
+                legend.decompose()
+
+            # Отправляем очищенный HTML в шаблон
+            return render(request, 'main/contact.html', {'schedule_html': result_element.prettify() })
+        else:
+            return render(request, 'main/contact.html', {'schedule_html': 'Элемент с классом "result" не найден.'})
+
+    except requests.exceptions.RequestException as e:
+        # Обработка ошибок, например, при сетевых проблемах
+        return render(request, 'main/contact.html', {'schedule_html': f'Ошибка при запросе: {str(e)}'})
