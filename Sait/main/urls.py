a3be7aa6d939d03a5ae1b743da5822d5612a0cc0
AnastasiaSurzhikowa <anastasia.surzhikowa@gmail.com>
2024-12-10 17:25:33 +0300

    Фикс изменений

--- Sait/main/urls.py
+++ Sait/main/urls.py
@@ -10,6 +10,17 @@ urlpatterns = [
     path('calendar', views.calendar, name = 'view_calendar'),
     path('mainpage', views.mainpage),
     path('contact', views.contact),
-    path('deadline', views.deadline),
-    path('calendar2', views.calendar2)
+    path('deadline', views.deadline, name = 'view_deadline'),
+    path('calendar2', views.calendar2, name = 'view_calendar2'),
+    path('schedule/', views.schedule_view1, {'offset': 0}, name='schedule'),
+    re_path(r'^schedule/(?P<offset>-?\d+)/$', views.schedule_view1, name='schedule_offset'),
+    path('schedule/add/', views.add_lesson, name='add_lesson'),
+    path('schedule/delete/<int:lesson_id>/', views.delete_lesson, name='delete_lesson'),
+    path('schedule/parse/', views.parse_schedule, name='parse_schedule'),
+
+    path('tasks/', views.task_view, {'offset': 0}, name='tasks'),
+    re_path(r'^task/(?P<offset>-?\d+)/$', views.task_view, name='tasks_offset'),
+    path('tasks/add/', views.add_task, name='add_task'),
+    path('tasks/delete/<int:task_id>/', views.delete_task, name='delete_task'),
+    
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Sait/main/views.py
+++ Sait/main/views.py
@@ -28,6 +28,220 @@ def deadline(request):
 def calendar2(request):
     return render(request, 'main/calendar2.html')
 
+def get_start_of_week(today):
+    start = today - timedelta(days=today.weekday())
+    return start
+
+def schedule_view1(request, offset="0"):
+    try:
+        offset = int(offset)
+    except ValueError:
+        offset = 0
+
+    today = date.today()  # Текущая дата
+    start_of_week = today + timedelta(weeks=offset)  # Дата начала недели
+    week_number = start_of_week.isocalendar()[1]  # Номер недели
+    parity = (week_number % 2 == 0)  # Четность недели
+
+    # Список дней недели с датами
+    days = [
+        {
+            'date': start_of_week + timedelta(days=i),
+            'day_name': day
+        }
+        for i, day in enumerate(['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС'])
+    ]
+
+    lessons = Lesson.objects.filter(week_parity=parity).order_by('day', 'start_time')
+
+    # Сортируем уроки по дням недели
+    lessons_by_day = {day['day_name']: [] for day in days}
+    for lesson in lessons:
+        lessons_by_day[lesson.day].append(lesson)
+
+    context = {
+        'days': days,  # Список дней с датами
+        'lessons_by_day': lessons_by_day,
+        'offset': offset,
+        'parity': 'Чётная' if parity else 'Нечётная',
+    }
+
+    return render(request, 'main/schedule.html', context)
+
+
+
+def add_lesson(request):
+    if request.method == 'POST':
+        # Получение данных из запроса
+        subject = request.POST.get('subject')
+        room = request.POST.get('room')
+        start_time = request.POST.get('start_time')
+        end_time = request.POST.get('end_time')
+        lesson_type = request.POST.get('lesson_type')
+        day = request.POST.get('day')
+        week_parity = request.POST.get('week_parity') == 'true'  # Преобразование строки в boolean
+
+        # Создание урока
+        lesson = Lesson.objects.create(
+            subject=subject,
+            room=room,
+            start_time=start_time,
+            end_time=end_time,
+            lesson_type=lesson_type,
+            day=day,
+            week_parity=week_parity
+        )
+
+        # Возврат JSON-ответа для AJAX-запросов
+        return JsonResponse({'success': True, 'lesson_id': lesson.id})
+
+    # Если метод GET — можно вернуть страницу добавления или редирект
+    return redirect('schedule')
+
+def delete_lesson(request, lesson_id):
+    Lesson.objects.filter(id=lesson_id).delete()
+    return JsonResponse({'success': True})
+
+#Задания
+
+def task_view(request, offset="0"):
+    try:
+        offset = int(offset)
+    except ValueError:
+        offset = 0
+
+    today = date.today()  # Текущая дата
+    start_of_week = today + timedelta(weeks=offset)  # Дата начала недели
+    week_number = start_of_week.isocalendar()[1]  # Номер недели
+    parity = (week_number % 2 == 0)  # Четность недели
+
+    # Список дней недели с датами
+    days = [
+        {
+            'date': start_of_week + timedelta(days=i),
+            'day_name': day
+        }
+        for i, day in enumerate(['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС'])
+    ]
+
+    tasks = Task.objects.filter(week_parity=parity).order_by('day', 'start_time')
+
+    # Сортируем уроки по дням недели
+    tasks_by_day = {day['day_name']: [] for day in days}
+    for task in tasks:
+        tasks_by_day[task.day].append(task)
+
+    context = {
+        'days': days,  # Список дней с датами
+        'tasks_by_day': tasks_by_day,
+        'offset': offset,
+        'parity': 'Чётная' if parity else 'Нечётная',
+    }
+
+    return render(request, 'main/schedule.html', context)
+
+
+
+def add_task(request):
+    if request.method == 'POST':
+        # Получение данных из запроса
+        subject = request.POST.get('subject')
+        descriptions = request.POST.get('descriptions')
+        start_time = request.POST.get('start_time')
+        task_type = request.POST.get('lesson_type')
+        day = request.POST.get('day')
+        week_parity = request.POST.get('week_parity') == 'true'  # Преобразование строки в boolean
+
+        # Создание урока
+        task = Lesson.objects.create(
+            subject=subject,
+            descriptions=descriptions,
+            start_time=start_time,
+            task_type=task_type,
+            day=day,
+            week_parity=week_parity
+        )
+
+        # Возврат JSON-ответа для AJAX-запросов
+        return JsonResponse({'success': True, 'task_id': task.id})
+
+    # Если метод GET — можно вернуть страницу добавления или редирект
+    return redirect('schedule')
+
+def delete_task(request, task_id):
+    Task.objects.filter(id=task_id).delete()
+    return JsonResponse({'success': True})
+
+#парсинг
+
+URL = "https://guap.ru/rasp/?g=319"
+
+def parse_schedule():
+    # Получаем HTML страницу
+    response = requests.get(URL)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    
+    # Находим все теги h3 с днями недели
+    days = soup.find_all('h3')
+    
+    for day in days:
+        day_name = day.get_text(strip=True)
+        
+        # Пропускаем, если нет дней недели в расписании
+        if day_name not in ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']:
+            continue
+        
+        # Находим все пары в этот день
+        lessons = day.find_next_siblings('div')  # Находим все div'ы, идущие после h3
+        
+        for lesson in lessons:
+            # Ищем время начала и конца пары
+            time_tag = lesson.find('h4')
+            if time_tag:
+                start_time, end_time = parse_time(time_tag.get_text(strip=True))
+            
+            # Ищем тип недели (четная/нечетная)
+            week_parity = True  # По умолчанию четная неделя
+            if lesson.find('b', class_='dn'):
+                week_parity = False
+            elif lesson.find('b', class_='up'):
+                week_parity = True
+            else:
+                week_parity = None  # Если ничего не указано, на обе недели
+            
+            # Получаем название предмета
+
+            # Получаем аудиторию
+            room_tag = lesson.find('em')
+            if room_tag:
+                room = room_tag.get_text(strip=True)
+            
+            # Ищем тип занятия
+            lesson_type_tag = lesson.find('b')
+            if lesson_type_tag:
+                lesson_type = lesson_type_tag.get_text(strip=True)
+            
+            # Сохраняем данные в модель Lesson
+            lesson_obj = Lesson(
+                day=day_name[:2],  # Сохраняем только первые две буквы дня недели
+                week_parity=week_parity,
+                subject=subject,
+                room=room,
+                start_time=start_time,
+                end_time=end_time,
+                lesson_type=lesson_type
+            )
+            lesson_obj.save()
+
+def parse_time(time_str):
+    # Преобразуем строку времени в объекты time
+    start_time_str, end_time_str = time_str.split(' - ')
+    start_time = time(*map(int, start_time_str.split(':')))
+    end_time = time(*map(int, end_time_str.split(':')))
+    return start_time, end_time
+
+
+
 def schedule_view(request):
     group_value = "317"
     group_value1 = "12312"  # Инициализация переменной по умолчанию
